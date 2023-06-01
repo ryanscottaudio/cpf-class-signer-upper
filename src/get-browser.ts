49fcfacc3a4b0157ac03c59212ca7ddf3b5a7f2e
Ryan Campbell <iamryancampbell@gmail.com>
2023-06-01 14:18:58 -0400

    Add return type to getBrowser function

--- src/get-browser.ts
+++ src/get-browser.ts
@@ -1,8 +1,8 @@
-import { chromium as playwright } from "playwright-core";
+import { Browser, chromium as playwright } from "playwright-core";
 import chromium from "@sparticuz/chromium";
 
-export const getBrowser = async (isLocal: boolean) =>
-  await playwright.launch({
+export const getBrowser = async (isLocal: boolean): Promise<Browser> =>
+  playwright.launch({
     args: isLocal ? undefined : chromium.args,
     executablePath: isLocal ? undefined : await chromium.executablePath(),
   });
